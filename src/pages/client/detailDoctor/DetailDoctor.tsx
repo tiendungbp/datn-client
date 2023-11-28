aabc561bc016718955a1308d68f061760966718a
Khanhnv2403 <vietkhanh.fe.work@gmail.com>
2023-11-29 01:26:01 +0700

    khanh - updated

--- src/pages/client/detailDoctor/DetailDoctor.tsx
+++ src/pages/client/detailDoctor/DetailDoctor.tsx
@@ -1,158 +1,158 @@
-import React, { useEffect, useState } from 'react';
-import { Breadcrumb, Col, Row } from 'antd';
-import { Link, NavLink, useParams } from 'react-router-dom';
-import { RootState, useAppDispatch } from '../../../store';
-import { useSelector } from 'react-redux';
-import { getAllDoctor } from '../../../services/managerDoctor';
-import DoctorSchedule from '../../../component/DoctorSchedule/DoctorSchedule';
-import { getOneDoctorStore } from '../../../store/managerDoctor.services/thunkAction';
-import { managerCategoryServices } from '../../../services/managerCategory';
-import CommonUtils from '../../../utils/commonUtils';
+import React, { useEffect, useState } from "react";
+import { Breadcrumb, Col, Row } from "antd";
+import { Link, NavLink, useParams } from "react-router-dom";
+import { RootState, useAppDispatch } from "../../../store";
+import { useSelector } from "react-redux";
+import { getAllDoctor } from "../../../services/managerDoctor";
+import DoctorSchedule from "../../../component/DoctorSchedule/DoctorSchedule";
+import { getOneDoctorStore } from "../../../store/managerDoctor.services/thunkAction";
+import { managerCategoryServices } from "../../../services/managerCategory";
+import CommonUtils from "../../../utils/commonUtils";
 
 const DetailDoctor = () => {
-	const { id } = useParams();
-	const Appdispatch = useAppDispatch();
-	const { doctor } = useSelector((state: RootState) => state.managerDoctor);
-	const [doctorCategories, setDoctorCategories] = useState([]);
+  const { id } = useParams();
+  const Appdispatch = useAppDispatch();
+  const { doctor } = useSelector((state: RootState) => state.managerDoctor);
+  const [doctorCategories, setDoctorCategories] = useState([]);
 
-	const [aDoctor, setADoctor] = useState<getAllDoctor>({
-		doctor_id: '',
-		fullname: '',
-		avatar: '',
-		dob: '',
-		gender: true,
-		phone: '',
-		degree: '',
-		start_date: null,
-		street: null,
-		ward: null,
-		district: null,
-		city: null,
-		html: '',
-		markdown: '',
-		email: '',
-		password: '',
-		is_activated: true,
-		is_blocked: true,
-		refresh_token: '',
-		createdAt: '',
-		updatedAt: '',
-	});
+  const [aDoctor, setADoctor] = useState<getAllDoctor>({
+    doctor_id: "",
+    fullname: "",
+    avatar: "",
+    dob: "",
+    gender: true,
+    phone: "",
+    degree: "",
+    start_date: null,
+    street: null,
+    ward: null,
+    district: null,
+    city: null,
+    html: "",
+    markdown: "",
+    email: "",
+    password: "",
+    is_activated: true,
+    is_blocked: true,
+    refresh_token: "",
+    createdAt: "",
+    updatedAt: "",
+  });
 
-	useEffect(() => {
-		fetchData(); // Gọi hàm fetchData khi component được render
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
-	const fetchData = async () => {
-		// Gửi yêu cầu lấy danh sách danh mục
-		window.scrollTo({ top: 0, behavior: 'smooth' });
-		await Appdispatch(getOneDoctorStore(id ? id : ''));
-		const res = await managerCategoryServices.getAllByDoctorID(id);
-		if (res.data.errCode === 0) {
-			setDoctorCategories(res.data.data);
-		}
-	};
-	useEffect(() => {
-		if (doctor) {
-			setADoctor(doctor?.data);
-		}
-	}, [doctor]);
-	return (
-		<div className="container_detailDoctor m-auto">
-			<Breadcrumb
-				className={`text-base mt-4`}
-				items={[
-					{
-						title: <NavLink to={'/'}>Trang chủ</NavLink>,
-					},
-					{
-						title: <NavLink to={'/services'}>Dịch vụ</NavLink>,
-					},
-					{
-						title: (
-							<span className="textColor">Bác sĩ {aDoctor?.fullname}</span>
-						),
-					},
-				]}
-			/>
-			<div className="flex flex-wrap justify-between">
-				<div className="content_left relative mt-10">
-					<img
-						className="img_doctor w-full relative"
-						src={aDoctor?.avatar}
-						alt={aDoctor?.fullname}
-					/>
-				</div>
-				<div className="content_right mt-24">
-					<h1 className=" font-bold text-2xl text-blue-500">
-						Bác sĩ {aDoctor?.fullname}
-					</h1>
-					<p className="mt-3 text-xl font-normal">
-						“ Mỗi khách hàng đối với tôi như người anh, chị, em trong gia đình.
-						Luôn mang đến cho họ những dịch vụ với kết quả tốt nhất là điều tôi
-						hằng tâm niệm”
-					</p>
-					<div className="text-xl mt-5">
-						<li>Bác sĩ Răng Hàm Mặt – Đại Học Y Dược TP Hồ Chí Minh 1996</li>
-						<li>
-							Chứng chỉ: Đào Tạo Implant Nha Khoa – Bệnh Viện Răng Hàm Mặt Trung
-							Ương TP Hồ Chí Minh 2011
-						</li>
-						<li>
-							Chứng chỉ: Chỉnh Nha Nâng Cao – Ortho Organizers, Inc – USA 2012
-						</li>
-						<li>
-							Chứng chỉ: Cập Nhật Thực Hành Implant Nha Khoa – Bệnh Viện Răng
-							Hàm Mặt Trung Ương TP Hồ Chí Minh 2015
-						</li>
-						<li>
-							Chứng chỉ: Chỉnh Hình Răng Mặt – Bệnh Viện Răng Hàm Mặt Trung Ương
-							TP Hồ Chí Minh 2018
-						</li>
-					</div>
-				</div>
-			</div>
-			<div className="bottom_detailDoctor mt-24 mb-20 flex-wrap">
-				<Row>
-					<Col md={6} xs={24}>
-						<span className="flex items-center mb-4 font-semibold text-lg">
-							<img src="./img/Group 232.png" alt="" />
-							Danh mục điều trị của bác sĩ
-						</span>
-						<table className="doctor-category-table">
-							<tbody>
-								{doctorCategories.map((item: any, index: any) => {
-									return (
-										<tr key={index}>
-											<td>{index + 1}</td>
-											<td className="capitalize text-blue-600">
-												<Link to={`/detailServices/${item.category_id}`}>
-													{CommonUtils.capitalizeEachWord(item.category_name)}
-												</Link>
-											</td>
-										</tr>
-									);
-								})}
-							</tbody>
-						</table>
-					</Col>
-					<Col md={6} xs={24} className="text-lg font-normal">
-						<span className="flex items-center font-semibold text-lg">
-							<img src="./img/Group 236.png" alt="" />
-							Địa chỉ và thời gian làm việc
-						</span>
-						<p className="mt-7">
-							Địa chỉ làm việc: Cơ sở 2 - 172 Trường chinh, <br /> Tân Thới
-							HIệp, Quận 12, Tp.HCM
-						</p>
-						<p>Thời gian: Từ 8:00 - 18:00 vào các ngày trong tuần</p>
-					</Col>
+  useEffect(() => {
+    fetchData(); // Gọi hàm fetchData khi component được render
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  const fetchData = async () => {
+    // Gửi yêu cầu lấy danh sách danh mục
+    window.scrollTo({ top: 0, behavior: "smooth" });
+    await Appdispatch(getOneDoctorStore(id ? id : ""));
+    const res = await managerCategoryServices.getAllByDoctorID(id);
+    if (res.data.errCode === 0) {
+      setDoctorCategories(res.data.data);
+    }
+  };
+  useEffect(() => {
+    if (doctor) {
+      setADoctor(doctor?.data);
+    }
+  }, [doctor]);
+  return (
+    <div className="container_detailDoctor m-auto">
+      <Breadcrumb
+        className={`md:text-sm text-xs`}
+        items={[
+          {
+            title: <NavLink to={"/"}>Trang chủ</NavLink>,
+          },
+          {
+            title: <NavLink to={"/services"}>Dịch vụ</NavLink>,
+          },
+          {
+            title: (
+              <span className="textColor">Bác sĩ {aDoctor?.fullname}</span>
+            ),
+          },
+        ]}
+      />
+      <div className="flex flex-wrap justify-between pl-1">
+        <div className="content_left relative mt-10">
+          <img
+            className="img_doctor w-full relative"
+            src={aDoctor?.avatar}
+            alt={aDoctor?.fullname}
+          />
+        </div>
+        <div className="content_right mt-24">
+          <h1 className=" font-bold text-2xl text-blue-500">
+            Bác sĩ {aDoctor?.fullname}
+          </h1>
+          <p className="mt-3 text-xl font-normal">
+            “ Mỗi khách hàng đối với tôi như người anh, chị, em trong gia đình.
+            Luôn mang đến cho họ những dịch vụ với kết quả tốt nhất là điều tôi
+            hằng tâm niệm”
+          </p>
+          <div className="text-xl mt-5">
+            <li>Bác sĩ Răng Hàm Mặt – Đại Học Y Dược TP Hồ Chí Minh 1996</li>
+            <li>
+              Chứng chỉ: Đào Tạo Implant Nha Khoa – Bệnh Viện Răng Hàm Mặt Trung
+              Ương TP Hồ Chí Minh 2011
+            </li>
+            <li>
+              Chứng chỉ: Chỉnh Nha Nâng Cao – Ortho Organizers, Inc – USA 2012
+            </li>
+            <li>
+              Chứng chỉ: Cập Nhật Thực Hành Implant Nha Khoa – Bệnh Viện Răng
+              Hàm Mặt Trung Ương TP Hồ Chí Minh 2015
+            </li>
+            <li>
+              Chứng chỉ: Chỉnh Hình Răng Mặt – Bệnh Viện Răng Hàm Mặt Trung Ương
+              TP Hồ Chí Minh 2018
+            </li>
+          </div>
+        </div>
+      </div>
+      <div className="bottom_detailDoctor mt-24 mb-20 flex-wrap">
+        <Row>
+          <Col md={6} xs={24}>
+            <span className="flex items-center mb-4 font-semibold text-lg">
+              <img src="./img/Group 232.png" alt="" />
+              Danh mục điều trị của bác sĩ
+            </span>
+            <table className="doctor-category-table">
+              <tbody>
+                {doctorCategories.map((item: any, index: any) => {
+                  return (
+                    <tr key={index}>
+                      <td>{index + 1}</td>
+                      <td className="capitalize text-blue-600">
+                        <Link to={`/detailServices/${item.category_id}`}>
+                          {CommonUtils.capitalizeEachWord(item.category_name)}
+                        </Link>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </Col>
+          <Col md={6} xs={24} className="text-lg font-normal">
+            <span className="flex items-center font-semibold text-lg">
+              <img src="./img/Group 236.png" alt="" />
+              Địa chỉ và thời gian làm việc
+            </span>
+            <p className="mt-7">
+              Địa chỉ làm việc: Cơ sở 2 - 172 Trường chinh, <br /> Tân Thới
+              HIệp, Quận 12, Tp.HCM
+            </p>
+            <p>Thời gian: Từ 8:00 - 18:00 vào các ngày trong tuần</p>
+          </Col>
 
-					{aDoctor.doctor_id && <DoctorSchedule doctor={aDoctor} />}
-				</Row>
-			</div>
-		</div>
-	);
+          {aDoctor.doctor_id && <DoctorSchedule doctor={aDoctor} />}
+        </Row>
+      </div>
+    </div>
+  );
 };
 
 export default DetailDoctor;
--- src/pages/client/detailServices/DetailServices.tsx
+++ src/pages/client/detailServices/DetailServices.tsx
@@ -1,166 +1,166 @@
-import { Breadcrumb, Spin } from 'antd';
-import Table, { ColumnsType } from 'antd/es/table';
-import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { NavLink, useNavigate, useParams } from 'react-router-dom';
-import { RootState, useAppDispatch } from '../../../store';
-import { getOneCategoryStore } from '../../../store/managerCategory.services/thunkAction';
-import { getAllServiceStore } from '../../../store/managerService.services/thunkAction';
-import { Vertical } from '../../../utils/AnimatedPage';
-import DoctorCard from '../../../component/DoctorCard/DoctorCard';
-import { getAllDoctorByCategoryService } from '../../../store/managerDoctor.services/thunkAction';
-import CommonUtils from '../../../utils/commonUtils';
+import { Breadcrumb, Spin } from "antd";
+import Table, { ColumnsType } from "antd/es/table";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { NavLink, useNavigate, useParams } from "react-router-dom";
+import { RootState, useAppDispatch } from "../../../store";
+import { getOneCategoryStore } from "../../../store/managerCategory.services/thunkAction";
+import { getAllServiceStore } from "../../../store/managerService.services/thunkAction";
+import { Vertical } from "../../../utils/AnimatedPage";
+import DoctorCard from "../../../component/DoctorCard/DoctorCard";
+import { getAllDoctorByCategoryService } from "../../../store/managerDoctor.services/thunkAction";
+import CommonUtils from "../../../utils/commonUtils";
 
 interface DataServiceProp {
-	key: React.Key;
-	name: string;
-	price: number;
-	guarantee: string;
-	description: string;
+  key: React.Key;
+  name: string;
+  price: number;
+  guarantee: string;
+  description: string;
 }
 
 const columnService: ColumnsType<DataServiceProp> = [
-	{
-		title: 'Dịch vụ',
-		dataIndex: 'name',
-		key: 'name',
-		render: (text: any) => CommonUtils.capitalizeEachWord(text),
-	},
-	{
-		title: 'Giá (VNĐ)',
-		dataIndex: 'price',
-		key: 'price',
-		align: 'center' as const,
-		render: (text: any) => CommonUtils.VND.format(text),
-	},
+  {
+    title: "Dịch vụ",
+    dataIndex: "name",
+    key: "name",
+    render: (text: any) => CommonUtils.capitalizeEachWord(text),
+  },
+  {
+    title: "Giá (VNĐ)",
+    dataIndex: "price",
+    key: "price",
+    align: "center" as const,
+    render: (text: any) => CommonUtils.VND.format(text),
+  },
 ];
 
 const DetailServices = () => {
-	const { id } = useParams();
-	const Appdispatch = useAppDispatch();
-	const { category } = useSelector((state: RootState) => state.managerCategory);
-	const navigate = useNavigate();
-	const { listService } = useSelector(
-		(state: RootState) => state.managerService,
-	);
-	const { listDoctor, isLoadingDoctor } = useSelector(
-		(state: RootState) => state.managerDoctor,
-	);
-	const [doctorList, setDoctorList] = useState<any>([]);
+  const { id } = useParams();
+  const Appdispatch = useAppDispatch();
+  const { category } = useSelector((state: RootState) => state.managerCategory);
+  const navigate = useNavigate();
+  const { listService } = useSelector(
+    (state: RootState) => state.managerService
+  );
+  const { listDoctor, isLoadingDoctor } = useSelector(
+    (state: RootState) => state.managerDoctor
+  );
+  const [doctorList, setDoctorList] = useState<any>([]);
 
-	useEffect(() => {
-		window.scrollTo({ top: 0, behavior: 'smooth' });
-		fetchData();
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
-	//XỬ LÝ LẤY CÁC BÁC SĨ ĐIỀU TRỊ DANH MỤC
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  //XỬ LÝ LẤY CÁC BÁC SĨ ĐIỀU TRỊ DANH MỤC
 
-	const fetchData = async () => {
-		// Gửi yêu cầu lấy danh sách danh mục
-		await Appdispatch(getOneCategoryStore(id ? id : ''));
-		// await Appdispatch(getDoctorsByCategoryID());
-		await Appdispatch(getAllDoctorByCategoryService(id ? id : ''));
-		await Appdispatch(getAllServiceStore());
-	};
-	useEffect(() => {
-		if (listDoctor) {
-			const { errCode } = listDoctor;
-			if (errCode === 0) {
-				setDoctorList(listDoctor.data);
-			} else {
-				navigate('/');
-			}
-		}
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [listDoctor]);
+  const fetchData = async () => {
+    // Gửi yêu cầu lấy danh sách danh mục
+    await Appdispatch(getOneCategoryStore(id ? id : ""));
+    // await Appdispatch(getDoctorsByCategoryID());
+    await Appdispatch(getAllDoctorByCategoryService(id ? id : ""));
+    await Appdispatch(getAllServiceStore());
+  };
+  useEffect(() => {
+    if (listDoctor) {
+      const { errCode } = listDoctor;
+      if (errCode === 0) {
+        setDoctorList(listDoctor.data);
+      } else {
+        navigate("/");
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [listDoctor]);
 
-	const dataService: DataServiceProp[] = listService
-		? listService
-				.filter((service: any) => {
-					return service.Category.category_id === category?.category_id;
-				})
-				.map((service: any, index: any) => ({
-					key: index,
-					name: service.service_name,
-					price: service.price,
-				}))
-		: [];
-	return (
-		<div className="m-auto w-wd-primary md:w-wd-secondary mt-12 lg:mt-[1rem]">
-			<Breadcrumb
-				className={`text-base`}
-				items={[
-					{
-						title: <NavLink to={'/'}>Trang chủ</NavLink>,
-					},
-					{
-						title: <NavLink to={'/services'}>Dịch vụ</NavLink>,
-					},
-					{
-						title: (
-							<span className="textColor capitalize">
-								{category?.category_name}
-							</span>
-						),
-					},
-				]}
-			/>
-			{/* nd title va mo ta cua dich vu */}
-			<div className="py-8">
-				<h1 className=" capitalize font-bold text-[1.2rem] md:text-[1.3rem] lg:text-[1.4rem] py-2  text-[#1386ED] ">
-					{category?.category_name}
-				</h1>
-				<span className='text-1rem md <Table columns={columns} dataSource={data} size="middle" />:text-[1.1rem] lg:text-[1.2rem] leading-9'>
-					Bọc răng sứ thẩm mỹ là kỹ thuật phục hình cố định bằng vật liệu sứ có
-					vai trò phục hồi chức năng ăn nhai, cải thiện thẩm mỹ giúp bạn tự tin
-					với nụ cười rạng rỡ tự nhiên. Những thắc mắc về Bọc răng sứ có đau
-					không? Có những loại răng sứ nào? Chi phí bọc răng sứ bao nhiêu?… sẽ
-					được giải đáp trong bài viết bên dưới. Bọc răng sứ thẩm mỹ là kỹ thuật
-					phục hình cố định bằng vật liệu sứ có vai trò phục hồi chức năng ăn
-					nhai, cải thiện thẩm mỹ giúp bạn tự tin với nụ cười rạng rỡ tự nhiên.
-					Những thắc mắc về Bọc răng sứ có đau không? Có những loại răng sứ nào?
-					Chi phí bọc răng sứ bao nhiêu?… sẽ được giải đáp trong bài viết bên
-					dưới. Bọc răng sứ thẩm mỹ là kỹ thuật phục hình cố định bằng vật liệu
-					sứ có vai trò phục hồi chức năng ăn nhai, cải thiện thẩm mỹ giúp bạn
-					tự tin với nụ cười rạng rỡ tự nhiên. Những thắc mắc về Bọc răng sứ có
-					đau không? Có những loại răng sứ nào? Chi phí bọc răng sứ bao nhiêu?…
-					sẽ được giải đáp trong bài viết bên dưới.
-				</span>
-			</div>
+  const dataService: DataServiceProp[] = listService
+    ? listService
+        .filter((service: any) => {
+          return service.Category.category_id === category?.category_id;
+        })
+        .map((service: any, index: any) => ({
+          key: index,
+          name: service.service_name,
+          price: service.price,
+        }))
+    : [];
+  return (
+    <div className="m-auto w-wd-primary md:w-wd-secondary mt-12 lg:mt-[1rem]">
+      <Breadcrumb
+        className={`md:text-sm text-xs`}
+        items={[
+          {
+            title: <NavLink to={"/"}>Trang chủ</NavLink>,
+          },
+          {
+            title: <NavLink to={"/services"}>Dịch vụ</NavLink>,
+          },
+          {
+            title: (
+              <span className="textColor capitalize">
+                {category?.category_name}
+              </span>
+            ),
+          },
+        ]}
+      />
+      {/* nd title va mo ta cua dich vu */}
+      <div className="py-8">
+        <h1 className=" capitalize font-bold text-[1.2rem] md:text-[1.3rem] lg:text-[1.4rem] py-2  text-[#1386ED] ">
+          {category?.category_name}
+        </h1>
+        <span className='text-1rem md <Table columns={columns} dataSource={data} size="middle" />:text-[1.1rem] lg:text-[1.2rem] leading-9'>
+          Bọc răng sứ thẩm mỹ là kỹ thuật phục hình cố định bằng vật liệu sứ có
+          vai trò phục hồi chức năng ăn nhai, cải thiện thẩm mỹ giúp bạn tự tin
+          với nụ cười rạng rỡ tự nhiên. Những thắc mắc về Bọc răng sứ có đau
+          không? Có những loại răng sứ nào? Chi phí bọc răng sứ bao nhiêu?… sẽ
+          được giải đáp trong bài viết bên dưới. Bọc răng sứ thẩm mỹ là kỹ thuật
+          phục hình cố định bằng vật liệu sứ có vai trò phục hồi chức năng ăn
+          nhai, cải thiện thẩm mỹ giúp bạn tự tin với nụ cười rạng rỡ tự nhiên.
+          Những thắc mắc về Bọc răng sứ có đau không? Có những loại răng sứ nào?
+          Chi phí bọc răng sứ bao nhiêu?… sẽ được giải đáp trong bài viết bên
+          dưới. Bọc răng sứ thẩm mỹ là kỹ thuật phục hình cố định bằng vật liệu
+          sứ có vai trò phục hồi chức năng ăn nhai, cải thiện thẩm mỹ giúp bạn
+          tự tin với nụ cười rạng rỡ tự nhiên. Những thắc mắc về Bọc răng sứ có
+          đau không? Có những loại răng sứ nào? Chi phí bọc răng sứ bao nhiêu?…
+          sẽ được giải đáp trong bài viết bên dưới.
+        </span>
+      </div>
 
-			{/* bang gia */}
-			<div className="w-70% md:w-[50%] m-auto py-8">
-				<Table
-					columns={columnService}
-					pagination={false}
-					dataSource={dataService}
-				/>
-			</div>
-			<Vertical>
-				<Spin tip="Đang tải..." spinning={isLoadingDoctor}>
-					<div className="container text-center p-5 title-category-detail">
-						<p style={{ fontSize: '16px' }}>
-							Danh sách các bác sĩ hiện đang điều trị danh mục
-						</p>
-						<h2 className="mb-3 capitalize text-blue-500">
-							{category?.category_name}
-						</h2>
-					</div>
-					<div className="container" style={{ backgroundColor: '#eff5ff' }}>
-						<div className="container pt-5">
-							<div className="row">
-								<div className="col-md">
-									{doctorList?.map((doctor: any, index: any) => {
-										return <DoctorCard key={index} doctor={doctor} />;
-									})}
-								</div>
-							</div>
-						</div>
-					</div>
-				</Spin>
-			</Vertical>
-		</div>
-	);
+      {/* bang gia */}
+      <div className="w-70% md:w-[50%] m-auto py-8">
+        <Table
+          columns={columnService}
+          pagination={false}
+          dataSource={dataService}
+        />
+      </div>
+      <Vertical>
+        <Spin tip="Đang tải..." spinning={isLoadingDoctor}>
+          <div className=" text-center p-5 title-category-detail">
+            <p style={{ fontSize: "16px" }}>
+              Danh sách các bác sĩ hiện đang điều trị danh mục
+            </p>
+            <h2 className="mb-3 capitalize text-blue-500">
+              {category?.category_name}
+            </h2>
+          </div>
+          <div style={{ backgroundColor: "#eff5ff" }}>
+            <div className="container pt-5">
+              <div className="row">
+                <div className="col-md">
+                  {doctorList?.map((doctor: any, index: any) => {
+                    return <DoctorCard key={index} doctor={doctor} />;
+                  })}
+                </div>
+              </div>
+            </div>
+          </div>
+        </Spin>
+      </Vertical>
+    </div>
+  );
 };
 
 export default DetailServices;
