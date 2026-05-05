a741b2517a15606114798f60fc7fcad287e09127
Do Cong Minh <160809416+doominh@users.noreply.github.com>
2026-05-05 22:21:56 +0700

    fix

--- src/component/Chatbot/ChatMessage.tsx
+++ src/component/Chatbot/ChatMessage.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
-import ChatbotIcon from "./ChatbotIcon";
 
 interface Message {
   role: string;
